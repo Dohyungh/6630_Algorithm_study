8708a8c104a5d824e5db32d77ef88f70988bbd1c
yngbao97 <yukyj0418@gmail.com>
2024-04-17 17:34:22 +0900

    예진 4/17 미확인도착지 코드 주석 추가

--- Calender/04_17_wed/1_Dijkstra/2_BOJ_9370_미확인도착지_예진/Main.java
+++ Calender/04_17_wed/1_Dijkstra/2_BOJ_9370_미확인도착지_예진/Main.java
@@ -1,5 +1,9 @@
 /*
- * [풀이 방법]
+ * [기존 풀이]
+ * 시작점으로부터 최단거리를 모두 구해서, 필수거리를 집합에서 잘라내 findSet으로 연결된 교차로를 찾으려고 했다.
+ * 하지만, 크루스칼, 프림, 다익스트라 모두 공통적으로 선택되는 간선 경우의 수가 항상 같지 않아서 문제가 된다.
+ *
+ * [재풀이 방법]
  * 1. 그래프의 인접 상태와 가중치를 입력받아 저장한다.
  * 2. 시작점으로부터 다익스트라를 실행한다. (각 노드까지의 거리 저장)
  * 3. 반드시 지나야 하는 거리의 양끝점 중 더 먼곳을 재시작점으로 지정한다. 
@@ -48,9 +52,10 @@ import java.util.Scanner;
 
 public class Main {
 	
+	// Integer.MAX_VALUE는 갱신때 다른 값을 더하면 이상한 값으로 변해서 문제에 주어진 가장 큰 값으로 지정했다.
 	static final int INF = 20_000_000;
-	static int[] fromSt;
-	static int[] fromRe;
+	static int[] fromSt;					// 시작점부터의 거리
+	static int[] fromRe;					// 재시작점부터의 거리
 	static boolean[] visited;
 	public static void main(String[] args) {
 
@@ -63,12 +68,16 @@ public class Main {
 			int m = sc.nextInt();	// 도로 수
 			int t = sc.nextInt();	// 목적지 후보 수
 			
-			int st = sc.nextInt();
+			int st = sc.nextInt();		// 시작지점
+
+			// 꼭 지나야 하는 거리의 양 끝점
 			int g = sc.nextInt();
 			int h = sc.nextInt();
 			
+			// 교차로는 번호가 1부터 존재해서 N+1 크기만큼 인접행렬생성
 			int[][] adj = new int[n+1][n+1];
 			
+			// 거리 정보 입력받기
 			for (int i = 0; i < m; i++) {
 				int A = sc.nextInt();
 				int B = sc.nextInt();
@@ -77,33 +86,46 @@ public class Main {
 				adj[A][B] = W;
 				adj[B][A] = W;
 			}
+
+			// 후보 도착지
 			int[] dep = new int[t];
 			
+			// 후보 도착지 입력
 			for (int i = 0; i < t; i++) {
 				dep[i] = sc.nextInt();
 			}
 			
+			// 시작점부터의 다익스트라 준비 
+			// ('모든 교차로까지의 최단거리'와 '꼭 지나야 하는 거리 중 더 나중에 가게될 교차로'를 찾기 위함)
 			fromSt = new int[n+1];
 			visited = new boolean[n+1];
 			
+			// 두 객체의 비교기준은 넣어줄 객체 안에서 찾아야함. 객체안의 인덱스로 외부 객체를 참조해서 비교하면 안됨.
+			// 예를 들어 int형일때, 참조할 수 있는 dp[] 배열이 있는 경우 dp[o1] - dp[o2] 이런식으로 하면 안됨.
+			// 외부 값이 변경됨에 따라 우선순위큐 내부 정렬이 달라지기 때문
 			PriorityQueue<int[]> queue = new PriorityQueue<>((o1, o2) -> {
 				return o1[1] - o2[1];
 			});
 			
-			
+			// 모든 거리 최대값으로 초기화
 			for (int i = 1; i <= n; i++) {
 				fromSt[i] = INF;
 			}
 			
+			// 출발점은 거리 0, 큐에 넣기(교차로 번호, 시작점부터의 거리)
 			fromSt[st] = 0;
 			queue.add(new int[] {st, fromSt[st]});
 			
+			// 다익스트라 실행
 			while (!queue.isEmpty()) {
+				// 출발점부터의 거리가 가장 짧은 거 하나 뽑아서
 				int curr = queue.poll()[0];
 				
+				// 이미 방문했으면 패스, 아니면 방문체크
 				if (visited[curr]) continue;
 				visited[curr] = true;
 				
+				// 인접한 교차로중 아직 최단거리를 확정짓지 않은 거리 정보 갱신, 큐에 넣기
 				for (int i = 1; i <= n; i++) {
 					if (adj[curr][i] != 0 && !visited[i] && 
 							fromSt[i] > fromSt[curr] + adj[curr][i]) {
@@ -113,24 +135,32 @@ public class Main {
 				}
 			}
 			
+			// 반드시 지나야 하는 거리의 끝점 즉, 재시작점 찾기
 			int reSt = 0;
 			List<Integer> answer = new ArrayList<>();
 			if (fromSt[g] > fromSt[h]) reSt = g;
 			else reSt = h;
 			
+
+			// 2차 다익스트라 준비
 			fromRe = new int[n+1];
 			visited = new boolean[n+1];
 			
+			// 큐 다시 초기화, 굳이 필요없을 것 같기도?
 			queue = new PriorityQueue<>((o1, o2) -> {
 				return o1[1] - o2[1];
 			});
 			
-			queue.add(new int[] {reSt, 0});
+			// 재시작점으로부터의 거리 최대값으로 초기화
 			for (int i = 1; i <= n; i++) {
 				fromRe[i] = INF;
 			}
+
+			// 재시작점 큐에 추가, 자기자신은 거리 0
+			queue.add(new int[] {reSt, 0});
 			fromRe[reSt] = 0;
 			
+			// 2차 다익스트라 실행 
 			while (!queue.isEmpty()) {
 				int curr = queue.poll()[0];
 				
@@ -147,12 +177,15 @@ public class Main {
 			}
 			
 			StringBuilder sb = new StringBuilder();
+
+			// 모든 도착후보지를 돌면서, 시작점부터의 거리와 필수거리를 지난 후 거리가 같으면 답에 추가
 			for (int i = 0; i < t; i++) {
 				if (fromSt[reSt]+fromRe[dep[i]] == fromSt[dep[i]]) {
 					answer.add(dep[i]);
 				}
 			}
 			
+			// 답안 오름차순 정렬 후 출력
 			Collections.sort(answer);
 			for (int a : answer) {
 				sb.append(a+" ");
