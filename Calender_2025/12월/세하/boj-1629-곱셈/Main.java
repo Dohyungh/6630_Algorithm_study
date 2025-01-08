6a0b9ba82de1066eeaee46221f490f0548ccbe14
sehaim <seha3722@gmail.com>
2025-01-09 03:18:25 +0900

    boj_1629_곱셈 README 추가

--- Calender_2025/12월/세하/boj-1629-곱셈/Main.java
+++ Calender_2025/12월/세하/boj-1629-곱셈/Main.java
@@ -4,17 +4,32 @@ import java.io.*;
 import java.util.*;
 
 public class Main {
-    public static void main(String[] args) throws IOException {
-        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        
-        long A = Integer.parseInt(st.nextToken());
-        long B = Integer.parseInt(st.nextToken());
-        long C = Integer.parseInt(st.nextToken());
-        
-        // 결과 계산 및 출력
-        long result = power(A, B, C);
-        System.out.println(result);
-        
-    }
+	public static void main(String[] args) throws IOException {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+
+		long A = Integer.parseInt(st.nextToken());
+		long B = Integer.parseInt(st.nextToken());
+		long C = Integer.parseInt(st.nextToken());
+
+		// 결과 계산 및 출력
+		long result = power(A, B, C);
+
+		System.out.println(result);
+
+	}
+
+	public static long power(long A, long B, long C) {
+		if (B == 1) {
+			return A % C;
+		}
+
+		long tmp = power(A, B / 2, C) % C;
+
+		if (B % 2 == 0) {
+			return tmp * tmp % C;
+		} else {
+			return tmp * tmp % C * A % C;
+		}
+	}
 }
