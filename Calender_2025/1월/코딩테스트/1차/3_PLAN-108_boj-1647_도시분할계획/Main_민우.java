068577936d496fd2b14b3a85e579bc4e42c1423a
FickleBoBo <yukmw0704@gmail.com>
2025-01-09 11:16:13 +0900

    20250109 코딩테스트

---
+++ Calender_2025/1월/코딩테스트/1차/3_PLAN-108_boj-1647_도시분할계획/Main_민우.java
@@ -0,0 +1,69 @@
+package day_09.BOJ_1647;
+
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    private static class Edge {
+        int a, b, c;
+
+        public Edge(int a, int b, int c) {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+    }
+
+    private static int[] p;
+
+    private static int[] make(int N) {
+        int[] arr = new int[1 + N];
+        for (int i = 1; i <= N; i++) arr[i] = i;
+        return arr;
+    }
+
+    private static int find(int x) {
+        if (x == p[x]) return x;
+        return p[x] = find(p[x]);
+    }
+
+    private static void union(int x, int y) {
+        p[find(y)] = find(x);
+    }
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int N = Integer.parseInt(st.nextToken());
+        int M = Integer.parseInt(st.nextToken());
+
+        PriorityQueue<Edge> edges = new PriorityQueue<>(((o1, o2) -> Integer.compare(o1.c, o2.c)));
+        for (int i = 0; i < M; i++) {
+            st = new StringTokenizer(br.readLine());
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+            int c = Integer.parseInt(st.nextToken());
+            edges.add(new Edge(a, b, c));
+        }
+
+        p = make(N);
+
+        int sum = 0;
+        int cnt = 0;
+        while (!edges.isEmpty()) {
+            if (cnt == N - 2) break;
+
+            Edge e = edges.poll();
+
+            if (find(e.a) == find(e.b)) continue;
+
+            union(e.a, e.b);
+            sum += e.c;
+            cnt++;
+        }
+
+        System.out.println(sum);
+    }
+}
