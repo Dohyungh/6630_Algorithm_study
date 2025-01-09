80288f61ea29e9b0852760ee598c237360ab0bfa
Dohyungh <bley1217@gmail.com>
2025-01-09 12:06:58 +0900

    수정

--- Calender_2025/1월/코딩테스트/1차/1_PLAN-71_boj-2630_색종이만들기/Main_도형.java
+++ Calender_2025/1월/코딩테스트/1차/1_PLAN-71_boj-2630_색종이만들기/Main_도형.java
@@ -1,80 +1,78 @@
-package BOJ._1647_도시분할계획;
+package BOJ._2630_색종이만들기;
 
-import java.util.PriorityQueue;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
 	
-	static class Edge implements Comparable<Edge> {
-		int no;
-		int de;
-		int val;
-		
-		Edge(int no, int de, int val) {
-			this.no = no;
-			this.de = de;
-			this.val = val;
-		}
-		
-		@Override
-		public int compareTo(Edge o) {
-		
-			return this.val - o.val;
-		}
-	}
-	
-	static int[] p;
+	static int wCnt = 0;
+	static int bCnt=  0;
 	
 	public static void main(String[] args) {
+		
 		Scanner sc = new Scanner(System.in);
 		
 		int N = sc.nextInt();
-		int M = sc.nextInt();
 		
-		if (N == 2) {
-			System.out.println(0);
+		int[][] arr = new int[N][N];
+		int[][] pSum = new int[N][N];
+		
+		for (int i = 0; i < N; i++) {
+			for (int j = 0; j < N; j++) {
+				arr[i][j] = sc.nextInt();
+			}
+		}
+		
+		pSum[0][0] = arr[0][0];
+		
+		for (int i = 1; i < N; i++) {
+			pSum[0][i] = pSum[0][i-1] + arr[0][i];
+			pSum[i][0] = pSum[i-1][0] + arr[i][0];
+		}
+		
+		for (int i = 1; i < N; i++) {
+			for (int j = 1; j < N; j++) {
+				pSum[i][j] = pSum[i-1][j] + pSum[i][j-1] - pSum[i-1][j-1] + arr[i][j];
+			}
+		}
+		getCounts(pSum, arr, 0, 0, N);
+		
+		System.out.println(wCnt);
+		System.out.println(bCnt);
+		
+		
+	}
+	
+	static void getCounts (int[][] pSum, int[][] arr,  int sRow, int sCol, int len) {
+		if (len == 1) {
+			if (arr[sRow][sCol] == 0) wCnt++;
+			else bCnt++;
 			return;
 		}
-		PriorityQueue<Edge> pq = new PriorityQueue<Edge>();
 		
-		for (int i = 0; i < M; i++) {
-			pq.add(new Edge(sc.nextInt(), sc.nextInt(), sc.nextInt()));
+		int eRow = sRow + len -1;
+		int eCol = sCol + len -1;
+		
+		int up = sRow -1 == -1? 0 : pSum[sRow -1][eCol];
+		int left= sCol -1 == -1? 0 : pSum[eRow][sCol-1];
+		int last= sRow -1 == -1 || sCol-1 == -1 ? 0 : pSum[sRow-1][sCol-1];
+		
+		int sum = pSum[eRow][eCol] - up - left + last;
+		
+		if (sum == len*len) {
+			bCnt++;
+			return;
 		}
-		
-		p = new int[N+1];
-		
-		for (int i = 1; i <= N; i++) {
-			p[i] = i;
+		else if (sum == 0) {
+			wCnt++;
+			return;
 		}
-		
-		int sum = 0;
-		int cnt = 0;
-		for (int i = 0; i < M; i++) {
-			
-			Edge curr = pq.poll();
-			
-			if (find(curr.no) == find(curr.de)) {
-				continue;
-			}
-			
-			union(curr.no, curr.de);
-			
-			sum += curr.val;
-			cnt++;
-			if (cnt == N-2) break;
+		else {
+			getCounts(pSum, arr, sRow, sCol, len/2);
+			getCounts(pSum, arr, sRow+len/2, sCol, len/2);
+			getCounts(pSum, arr, sRow, sCol+len/2, len/2);
+			getCounts(pSum, arr, sRow+len/2, sCol+len/2, len/2);
 		}
-		System.out.println(sum);
-	}
-	
-	static int find(int idx) {
-		if (p[idx] == idx) return idx;
-		
-		return p[idx] = find(p[idx]);
-	}
-	
-	static void union(int no, int de) {	
-		
-		p[find(no)] = find(de);
 	}
 
 }
